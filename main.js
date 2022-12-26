316557f4e667b134ce9bb9352ec4799c0747deb4
Q-Gaata <alabigata@gmail.com>
2022-12-26 18:04:42 +0100

    Complete activities

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('.nav-cont')
     const navLi = document.querySelectorAll('nav li a')
@@ -16,3 +32,4 @@ window.addEventListener('scroll', () => {
     btn.style.color = '#002147'
     nav.classList.toggle('scroll', window.scrollY > 50)
 })
+
